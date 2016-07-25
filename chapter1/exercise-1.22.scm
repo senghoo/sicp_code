f0976fc2d1b65baa014c25533edbaa12e4d4dd33
Senghoo Kim <shkdmb@gmail.com>
2016-07-25 09:54:27 +0800

    1.22

---
+++ chapter1/exercise-1.22.scm
@@ -0,0 +1,34 @@
+(define (square n) (* n n))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((devides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (devides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes min max)
+  (cond ((even? min) (search-for-primes (+ min 1) max))
+        ((< min max)
+         (timed-prime-test min)
+         (search-for-primes (+ min 2) max))))
