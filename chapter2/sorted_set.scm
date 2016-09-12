4719103f6f6662747b6b8210f9ee258765cf1c1b
Senghoo Kim <shkdmb@gmail.com>
2016-09-12 22:57:45 +0800

    union-set

--- chapter2/sorted_set.scm
+++ chapter2/sorted_set.scm
@@ -23,3 +23,13 @@
         ((= x (car set)) set)
         ((< x (car set)) (cons x set))
         (else (cons (car set) (adjoin-set x (cdr set))))))
+
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((= (car set1) (car set2))
+         (cons (car set1) (union-set (cdr set1) (cdr set2))))
+        ((< (car set1) (car set2))
+         (cons (car set1) (union-set (cdr set1) set2)))
+        (else (cons (car set2) (union-set set1 (cdr set2))))))
