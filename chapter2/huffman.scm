7688caff73640a1b487f7986bbe4171bc333b392
Senghoo Kim <shkdmb@gmail.com>
2016-09-16 18:45:37 +0800

    encode

--- chapter2/huffman.scm
+++ chapter2/huffman.scm
@@ -48,3 +48,16 @@
                                (cadr pair)) ; frequency
                     (make-leaf-set (cdr pairs))))))
 
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (encode-symbol symbol tree)
+  (cond ((leaf? tree) '())
+        ((memq symbol (symbols (left-branch tree)))
+         (cons 0 (encode-symbol symbol (left-branch tree))))
+        ((memq symbol (symbols (right-branch tree)))
+         (cons 1 (encode-symbol symbol (right-branch tree))))
+        (else (error "bad symbol: " symbol))))
