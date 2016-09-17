891f5381d3a5923e5755f6a0b9c44c627a2a49b3
Senghoo Kim <shkdmb@gmail.com>
2016-09-17 17:02:04 +0800

    successive-merge

--- chapter2/huffman.scm
+++ chapter2/huffman.scm
@@ -61,3 +61,17 @@
         ((memq symbol (symbols (right-branch tree)))
          (cons 1 (encode-symbol symbol (right-branch tree))))
         (else (error "bad symbol: " symbol))))
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge ordered-set)
+  (cond ((= 0 (length ordered-set))
+         '())
+        ((= 1 (length ordered-set))
+         (car ordered-set))
+        (else
+         (let ((new-sub-tree (make-code-tree (car ordered-set)
+                                             (cadr ordered-set)))
+               (remained-ordered-set (cddr ordered-set)))
+           (successive-merge (adjoin-set new-sub-tree remained-ordered-set))))))
